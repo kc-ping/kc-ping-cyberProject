8f052aa030b77a032fe2bae48133927f2facdfe5
kc-ping <kezynganou@gmail.com>
2025-10-22 07:15:06 +0200

    UPDATE BEGIN

--- beginner/PacketSniffer/sniffer_demo.py
+++ beginner/PacketSniffer/sniffer_demo.py
@@ -25,7 +25,35 @@ def get_mac_addr(bytes_addr):
     print(bytes_str)
     return ':'.join(bytes_str).upper()
 
+#unpack IPv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version= version_header_length >> 4 # bitwise operation to extract version
+    header_length = (version_header_length & 15) * 4 # bitwise operation to extract header length
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20]) # unpack ipv4 header
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
 
+# Returns properly formatted IPv4 address
+def ipv4(addr):
+    print(addr)
+    return '.'.join(map(str, addr)) # convert bytes to decimal format and join with '.'
+
+# Unpack ICMP(Internet Control Message Protocol) packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4]) # unpack icmp header
+    return icmp_type, code, checksum, data[4:]
+
+#unpack TCP segment
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14]) # unpack tcp header
+    offset = (offset_reserved_flags >> 12) * 4 # bitwise operation to extract offset
+    flag_urg = (offset_reserved_flags & 32) >> 5 # bitwise operation to extract flags
+    flag_ack = (offset_reserved_flags & 16) >> 4 # bitwise operation to extract flags
+    flag_psh = (offset_reserved_flags & 8) >> 3 # bitwise operation to extract flags
+    flag_rst = (offset_reserved_flags & 4) >> 2 # bitwise operation to extract flags
+    flag_syn = (offset_reserved_flags & 2) >> 1 # bitwise operation to extract flags
+    flag_fin = (offset_reserved_flags & 1) # bitwise operation to extract flags
+    return src_port, dest_port, sequence, acknowledgment, offset, data[offset:], flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin
 
 
 
